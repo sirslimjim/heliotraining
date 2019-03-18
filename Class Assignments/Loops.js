e26ec3bc88a3b48f352e23eac8c446cd1a2810dc
Eric Garner <eric.garner@HC2557.hqcatalyst.local>
2019-03-18 10:29:02 -0600

    Adding loops assignment to master branch

---
+++ Class Assignments/Loops.js
@@ -0,0 +1,20 @@
+let numberArray = [
+754,
+22,
+621,
+4,
+5,
+63,
+73,
+1,
+84,
+]
+
+function forLoopFunction(arrayofStuff){
+    for(i=0;i<arrayofStuff.length;i++){
+        console.log(`The current object at position ${i} is ${arrayofStuff[i]}`)
+    }
+}
+
+
+forLoopFunction(numberArray);
